30d668a389e991910fc5909910d69cb17fdef2d1
siamshahriar <siamshahriar99@gmail.com>
2022-09-14 22:06:26 +0600

    start button fixed

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+document.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
